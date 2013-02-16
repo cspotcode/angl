554f3d634131ae2b0647530ad99710dda8bd66cf
Andrew Bradley <cspotcode@gmail.com>
2013-02-16 16:40:00 -0500

    Switched to node-powered build script.  It's short(ish), sweet, and does everything the build.sh did, but more cross-platform and robust

---
+++ build.js
@@ -0,0 +1,36 @@
+var child_process = require('child_process');
+var fs = require('fs');
+var path = require('path');
+var executive = require('executive');
+require('shelljs/global');
+
+// Utility to combine arguments into a command line
+var buildCommandLine = function(/* ... args */) {
+    return [].slice.apply(arguments).map(function(v) {return v.replace(/ /g, '\\ ')}).join(' ');
+};
+
+// Let's get down to business
+
+echo('Building angl parser...');
+
+cd(__dirname);
+
+// Create empty output directory
+rm('-rf', 'out');
+mkdir('out');
+
+// Grab the path to Jison's command-line tool
+var jisonBin = path.resolve(require.resolve('jison/package.json'), '..', require('jison/package.json').bin.jison);
+
+// Build the parser
+var command = buildCommandLine(process.argv[0], jisonBin, 'src/parser.jison', '-o', 'out/parser.js');
+executive(command, function(err, out, code) {
+    if(err) throw err;
+
+    // Copy js source to output
+    cp('src/*.js', 'out');
+
+    echo('Done!');
+});
+
+
