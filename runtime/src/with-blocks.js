19da575d341df0f901390973e03bd489f1cd73cd
Andrew Bradley <cspotcode@gmail.com>
2013-05-14 22:38:14 -0400

    Committed stubbed-out runtime support for with() loops

---
+++ runtime/src/with-blocks.js
@@ -0,0 +1,10 @@
+// Runtime behavior to support Angl's `with()` loops
+
+module.exports.resolveWithExpression = function(value) {
+    // TODO implement me
+    // should accept a value that might be an instance of an AnglObject, an AnglObject, or something else
+    // If something else, throw an error.  (GML throws an error when given a string, for example)
+    // Return an array containing:
+    //   the instance, if passed an instance
+    //   an array of all instances of the AnglObject, if passed an AnglObject
+}
