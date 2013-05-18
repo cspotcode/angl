5020bf601cfa9931e7f92d3ae4ecb9b9f12e45d7
Andrew Bradley <cspotcode@gmail.com>
2013-05-18 10:38:59 -0400

    Compiler's global scope now populates itself by loading the runtime, then looking at what has been added to the global scope

--- compiler/lib/global-scope.ts
+++ compiler/lib/global-scope.ts
@@ -1,15 +1,19 @@
+var _ = require('lodash');
 import scope = module('./angl-scope');
 import scopeVariable = module('./scope-variable');
 import strings = module('./strings');
-var _ = require('lodash');
+var anglGlobalsNamespace = require('runtime/angl-globals-namespace');
+// Trigger loading of all globals onto the globals namespace
+require('runtime/angl-globals');
+
 
 export function createGlobalScope():scope.AnglScope {
     var globalScope = new scope.AnglScope();
 
-    var globalIdentifiers:any = 'global true false';
+    // grab the list of all global identifiers from the runtime
+    var globalIdentifiers:any = _.keys(anglGlobalsNamespace);
 
     // Add all global identifiers into global scope
-    globalIdentifiers = globalIdentifiers.split(' ');
     _.each(globalIdentifiers, (globalIdentifier) => {
         // TODO what values should I be adding?  Gotta invent an object/type/schema for values.
         var variable = new scopeVariable.Variable(globalIdentifier, 'PROP_ASSIGNMENT', 'PROP_ACCESS');
