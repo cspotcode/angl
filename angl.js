0410b0355ff3da304e82c65f6a270a26cb532fb6
Andrew Bradley <abradley@mitre.org>
2013-06-07 20:50:22 -0400

    Updated command-line interface using nomnom; provides robust option parsing and --help usage info

--- angl.js
+++ angl.js
@@ -1,4 +1,5 @@
 var fs = require('fs'),
+    nomnom = require('nomnom'),
     parser = require('./parser/out/angl'),
     compiler = require('./compiler/lib/compiler');
 
@@ -7,10 +8,43 @@ module.exports = {
     compiler: compiler
 };
 
+var cliParser = nomnom();
+cliParser.script('angl');
+
+cliParser.command('parse')
+    .help('Parse an Angl file into an AST.')
+    .callback(function(opts) {
+        parser.printAST(fs.readFileSync(opts._[1]).toString());
+    });
+
+cliParser.command('compile')
+    .help('Compile Angl into JavaScript.')
+    .options({
+        'directory': {
+            position: 1,
+            help: 'Directory containing Angl source files to compile.',
+            required: true,
+            meta: 'DIRECTORY',
+            type: 'string'
+        }
+    })
+    .callback(function(opts) {
+        var directoryPath = opts.directory;
+        compiler.compileDirectory(directoryPath, function(err) {
+            if(err) {
+                console.error('Compilation error:');
+                console.error(err.message);
+                process.exit(1);
+            }
+            console.log('Done!');
+        });
+    });
+
+cliParser.parse();
+
 // command line
 if (require.main === module) {
     if (process.argv.hasOwnProperty('2') && process.argv[2] !== '--help') {
-        parser.printAST(fs.readFileSync(process.argv[2]).toString());
     } else {
         console.log('Usage:');
         console.log('   node angl.js FILENAME');
