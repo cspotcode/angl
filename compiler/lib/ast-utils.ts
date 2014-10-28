cf598349cb1d322dc4caed5d78b51290be147106
Andrew Bradley <cspotcode@gmail.com>
2014-10-28 03:37:12 -0400

    Code cleanup

--- compiler/lib/ast-utils.ts
+++ compiler/lib/ast-utils.ts
@@ -6,22 +6,22 @@ import anglScope = require('./angl-scope');
 
 // "Cleans" an AST node in preparation for moving it in the AST tree.
 // The tree walker will take care of re-assigning the necessary properties based on the node's new tree position.
-export var cleanNode = (astNode:astTypes.AstNode) => {
+export function cleanNode(astNode: astTypes.AstNode) {
     astNode.parentNode = null;
     return astNode;
 }
 
-export var getAnglScope = (astNode:astTypes.AstNode):anglScope.AnglScope => {
+export function getAnglScope(astNode: astTypes.AstNode): anglScope.AnglScope {
     while(!astNode.anglScope) astNode = astNode.parentNode;
     return astNode.anglScope;
 }
 
-export var getGlobalAnglScope = (astNode:astTypes.AstNode):anglScope.AnglScope => {
+export function getGlobalAnglScope(astNode: astTypes.AstNode): anglScope.AnglScope {
     while(!astNode.globalAnglScope) astNode = astNode.parentNode;
     return astNode.globalAnglScope;
 }
 
-export var findParent = (astNode:astTypes.AstNode, callback:(astNode:astTypes.AstNode)=>boolean, includeStartNode: boolean = false):astTypes.AstNode => {
+export function findParent(astNode: astTypes.AstNode, callback: (astNode: astTypes.AstNode) => boolean, includeStartNode: boolean = false): astTypes.AstNode {
     // If we are *not* considering the current node as a possible candidate, skip it right away and start the search with its parent.
     if(!includeStartNode) astNode = astNode.parentNode;
     while(true) {
