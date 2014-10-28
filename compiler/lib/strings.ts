886a2f213ccac55f38dbd03a9aef33263813ede5
Andrew Bradley <cspotcode@gmail.com>
2014-10-28 03:41:08 -0400

    Changes module names for angl globals and runtime

--- compiler/lib/strings.ts
+++ compiler/lib/strings.ts
@@ -3,12 +3,10 @@
 
 // variable name and RequireJS module name for the Angl global namespace
 export var ANGL_GLOBALS_IDENTIFIER = '$AG';
-export var ANGL_GLOBALS_MODULE = 'angl-globals-namespace';
+export var ANGL_GLOBALS_MODULE = 'angl/globals';
 // variable name and RequireJS module name for the Angl runtime
 export var ANGL_RUNTIME_IDENTIFIER = '$ART';
-export var ANGL_RUNTIME_MODULE = 'angl-runtime';
-// Prefix for the RequireJS module name of a compiled Angl file
-export var ANGL_FILE_MODULE_PREFIX = 'compiled-angl/';
+export var ANGL_RUNTIME_MODULE = 'angl/runtime';
 // Name of the default super-object of all Angl objects
 export var SUPER_OBJECT_NAME = '$AnglObject';
 // Name of the method used to initialize instance properties upon creation
