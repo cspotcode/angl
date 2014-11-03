dc27286bcebdcfefed36ddd8badecadfde22e865
Andrew Bradley <cspotcode@gmail.com>
2014-11-02 22:00:54 -0500

    Expands compiler API to allow specifying more options via option-bag configuration objects

--- compiler/lib/compiler.ts
+++ compiler/lib/compiler.ts
@@ -15,6 +15,7 @@ import angl = require('./angl-parser');
 
 import astTypes = require('./ast-types');
 import globalScope = require('./global-scope');
+import scopeVariable = require('./scope-variable');
 import allTransformations = require('./run-all-transformations');
 import findGlobals = require('./find-globals');
 import jsGenerator = require('./main');
@@ -24,18 +25,79 @@ var Ident = identifierManipulations.Identifier;
 
 var defaultOptions = new options.Options();
 
-export function compile(anglSourceCode:string, options: options.Options):string {
 function normalizeNewlines(input: string): string {
     return input.replace(/\r\n/g, '\n');
 }
 
+export interface AbstractCompileOptions {
+    /**
+     * Compiler options.
+     */
+    options?: options.Options;
+    /**
+     * The name of a module that can be require'd to create the globalVariables array.
+     */
+    globalVariablesModule?: string;
+    /**
+     * Array of globalVariables to be added to global scope before compiling code.
+     */
+    globalVariables?: Array<scopeVariable.AbstractVariable>;
+    /**
+     * TODO
+     */
+    resources?: {
+        sprites?: Array<string>;
+        backgrounds?: Array<string>;
+        sounds?: Array<string>;
+    };
+}
+
+export interface CompileOptions extends AbstractCompileOptions {
+    anglSourceCode: string;
+}
+
+export function compile(opts: CompileOptions): string;
+export function compile(anglSourceCode: string, options: options.Options): string;
+export function compile(arg0: any, arg1?: any):string {
+    var anglSourceCode: string, options: options.Options, opts: CompileOptions;
+    if(typeof arg0 === 'object') {
+        opts = arg0;
+        anglSourceCode = opts.anglSourceCode;
+        options = opts.options;
+    } else {
+        anglSourceCode = arg0;
+        options = arg1;
+    }
     // Parse the angl source code into an AST
     anglSourceCode = normalizeNewlines(anglSourceCode);
     var ast = angl.parse(anglSourceCode);
     return compileAst(ast, null, options);
 }
 
-export function compileAst(anglAst:astTypes.AstNode, extraGlobalIdentifiers:string[] = [], options: options.Options = defaultOptions):string {
+export interface CompileAstOptions extends AbstractCompileOptions {
+    anglAst: astTypes.AstNode;
+    extraGlobalIdentifiers: Array<string>;
+}
+
+export function compileAst(opts: CompileAstOptions): string;
+export function compileAst(anglAst: astTypes.AstNode, extraGlobalIdentifiers?: Array<string>, options?: options.Options): string;
+export function compileAst(arg0: any, arg1?: any, arg2?: any): string {
+    var anglAst: astTypes.AstNode,
+        extraGlobalIdentifiers: Array<string>,
+        options: options.Options,
+        opts: CompileAstOptions;
+    if(typeof arg0.type === 'string') {
+        // arg0 is an ast
+        anglAst = arg0;
+        extraGlobalIdentifiers = typeof arg1 === 'undefined' ? [] : arg1;
+        options = typeof arg2 === 'undefined' ? defaultOptions : arg2;
+    } else {
+        opts = arg0;
+        anglAst = opts.anglAst;
+        extraGlobalIdentifiers = opts.extraGlobalIdentifiers || [];
+        options = opts.options || defaultOptions;
+    }
+    
     // Manually create and assign a global scope to the AST
     var newGlobalScope = globalScope.createGlobalScope(options, extraGlobalIdentifiers);
     anglAst.globalAnglScope = newGlobalScope;
@@ -53,7 +115,27 @@ interface AnglFile {
     compiledJs?: string;
 }
 
-export function compileDirectory(sourcePath: string, destinationPath: string, options: options.Options = defaultOptions) {
+export interface CompileDirectoryOptions extends AbstractCompileOptions {
+    src: string;
+    dest: string;
+}
+
+export function compileDirectory(opts: CompileDirectoryOptions);
+export function compileDirectory(sourcePath: string, destinationPath: string, options: options.Options);
+export function compileDirectory(arg0: any, arg1?: any, arg2?: any) {
+    var opts: CompileDirectoryOptions;
+    if(typeof arg0 === 'object') {
+        opts = arg0;
+    } else {
+        opts = {
+            src: <string>arg0,
+            dest: <string>arg1,
+            options: <options.Options>arg2
+        }
+    }
+    var sourcePath = opts.src,
+        destinationPath = opts.dest,
+        options = opts.options || defaultOptions;
     
     if(!fs.statSync(sourcePath).isDirectory()) throw new Error('"' + sourcePath + '" is not a directory.');
     
@@ -76,7 +158,7 @@ export function compileDirectory(sourcePath: string, destinationPath: string, op
     });
     
     // Create a global scope
-    var newGlobalScope = globalScope.createGlobalScope(options);
+    var newGlobalScope = globalScope.createGlobalScope(options, undefined, opts.globalVariables || []);
     
     console.log('Performing first transformation phase on each file...');
     var allFileAsts = <Array<astTypes.FileNode>>_.map(files, (file) => {
